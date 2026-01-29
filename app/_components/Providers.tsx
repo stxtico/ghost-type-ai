d32f3aaf4cbc7c4e361c2acce471dc05837f3b4b
Allen <allenalv082009@gmail.com>
2026-01-29 18:38:25 -0500

    done

--- app/_components/Providers.tsx
+++ app/_components/Providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import ThemeProvider from "./ThemeProvider";
+import React from "react";
+import { ThemeProvider } from "./ThemeProvider";
 import { LanguageProvider } from "./LanguageProvider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
